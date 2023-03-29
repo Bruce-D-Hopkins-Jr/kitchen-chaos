c1bbfe43b6c9745965620d1100b823174edb133e
Bruce D. Hopkins Jr <brucedenhopk@gmail.com>
2023-03-28 22:19:26 -0400

    Animations
    Added animations to Player for better visuals.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,25 +8,27 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 12f;
     [SerializeField] private float rotationSpeed = 5f;
 
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.W))
         {
-            inputVector.y = +1;            
+            inputVector.y = +1;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            inputVector.x = -1;            
+            inputVector.x = -1;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            inputVector.y = -1;            
+            inputVector.y = -1;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            inputVector.x = +1;            
+            inputVector.x = +1;
         }
 
         inputVector = inputVector.normalized;
@@ -34,6 +36,13 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+    }
+
+    public bool IsWalking() 
+    {
+        return isWalking;
     }
 }
